9291dfa1455d063ff052f9e7afd740ce83f59cc0
Abe <alopezosorio04@gmail.com>
2023-05-02 20:06:51 -0400

    Updated navbar (again lol) Trying to add hamburger to navbar for mobile

---
+++ src/main.js
@@ -0,0 +1,23 @@
+document.getElementById("yv").addEventListener("click", function(){
+    document.getElementById("airhorn").play();
+});
+
+
+
+//navbar script
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
